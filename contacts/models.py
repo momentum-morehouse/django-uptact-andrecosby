c26c25bf3d0ff05f1d8c8486c1da9d969ad9f8ac
Deandre Cosby <andrecosby1@gmail.com>
2020-10-19 15:28:33 -0400

    update

--- contacts/models.py
+++ contacts/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
         message="Phone number must be entered in the format: '+9999999999'.")
 
     name = models.CharField(max_length=255)
-    birthday = models.datefield(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     phone_number = models.CharField(max_length=11,
                                     validators= [phone_regex],
@@ -20,3 +20,4 @@ class Contact(models.Model):
     city = models.CharField(max_length=255, null=True, blank=True)
     state = USStateField(null=True, blank=True)
     zip_code = USZipCodeField(null=True, blank=True)
+
